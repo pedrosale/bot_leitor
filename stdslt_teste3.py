409ebfc9916dfa1fb0ae4bddec3a3d0e01f4cbf3
pedrosale <53319245+pedrosale@users.noreply.github.com>
2024-02-26 13:56:29 -0300

    Update stdslt_teste3.py

--- stdslt_teste3.py
+++ stdslt_teste3.py
@@ -1,38 +1,41 @@
+import openai
 import streamlit as st
-from streamlit_chat import message
-from langchain.chains import ConversationalRetrievalChain
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import Replicate
-from langchain.text_splitter import CharacterTextSplitter
+import pandas as pd
+from transformers import GPT2Tokenizer
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.memory import ConversationBufferMemory
-from langchain.document_loaders import TextLoader
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.chains import ConversationalRetrievalChain
 import os
-from dotenv import load_dotenv
-import tempfile
-import urllib.request
-
-
 
 # Configurações do ChatBot
 st.title("Este é o ChatBot desenvolvido por Pedro Sampaio Amorim. Inclua um texto para debater com o bot!")
 
-# Carrega o texto diretamente de um link
-file_path1 = "https://raw.githubusercontent.com/pedrosale/falcon_test/main/CTB3.txt"
-with tempfile.NamedTemporaryFile(delete=False) as temp_file1:
-    temp_file1.write(urllib.request.urlopen(file_path1).read())
-    temp_file_path1 = temp_file1.name
+openai.api_key = st.secrets['OPENAI_API_KEY']
 
-text1 = []
-loader1 = TextLoader(temp_file_path1)
-text1.extend(loader1.load())
-os.remove(temp_file_path1)
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-# Exibe o histórico de mensagens
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Verifica se um arquivo foi enviado
+arquivo_upload = st.file_uploader("Escolha um arquivo TXT", type=["txt"])
+if arquivo_upload is not None:
+    st.success("Arquivo enviado com sucesso!")
+
+    # Lê o conteúdo do arquivo enviado
+    conteudo_bytes = arquivo_upload.read()
+    conteudo = conteudo_bytes.decode('utf-8')  # Converte bytes para string
+
+    # Recebe a entrada do usuário do arquivo enviado (Tipo 1)
+    prompt_tipo_1 = conteudo
+    st.session_state.messages.append({"role": "user", "content": prompt_tipo_1, "tipo": "tipo_1"})
+
+# Exibe o histórico de mensagens
 for message in st.session_state.messages:
     if message["role"] == "assistant":
         with st.chat_message("assistant"):
@@ -44,27 +47,23 @@ for message in st.session_state.messages:
         with st.chat_message("user_tipo_2"):
             st.markdown(f"**Usuário:** {message['content']}")
 
-# Dividir o texto em chunks
-text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=950, length_function=len)
-text_chunks = text_splitter.split_documents(text1)
-
-# Criar embeddings para os chunks
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
-
-# Criar vector store com os embeddings
-vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
-
-# Criar a cadeia conversacional
-chain = ConversationalRetrievalChain(vector_store)
-
 # Recebe a entrada do usuário (Tipo 2)
-prompt_tipo_2 = st.text_input("Enviou o texto? Se sim, o que você gostaria de discutir sobre ele? Caso não queira falar sobre texto, do que deseja falar?")
-
-if prompt_tipo_2:
+if prompt_tipo_2 := st.text_input("Enviou o texto ? Se sim, o que você gostaria de discutir sobre ele? Caso não queira falar sobre texto, do que deseja falar?"):
     st.session_state.messages.append({"role": "user", "content": prompt_tipo_2, "tipo": "tipo_2"})
 
     # Gera a resposta do ChatBot
     with st.chat_message("assistant"):
-        response = chain.get_response(prompt_tipo_2)
-        st.write(response)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
